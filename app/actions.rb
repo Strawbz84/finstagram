36fdebbef01348dcadca7d3427703200f8536664
Patrice <patrice.watts@hotmail.com>
2023-11-23 03:28:08 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,29 @@
 get '/' do
-    File.read(File.join('app/views', 'index.html'))
+    
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
+    erb(:index)
+end
+
+get '/signup' do        # if a user navigates to the path "/signup"
+    @user = User.new    # setup empty @user object
+    erb(:signup)        # render "app/views/signup.erb"
+end
+
+post '/signup' do 
+         # grab user input values from params
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  # instantiate a User
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  # if user validations pass and user is saved
+  if @user.save
+    "User.new #{username} saved!"
+  else
+    erb(:signup)
+
+  end
 end
